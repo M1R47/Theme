a9a15861664336a5787af57545fe0a449bd402d8
Dmonk <106853330+M1R47@users.noreply.github.com>
2023-02-16 14:38:35 -0500

    Add files via upload

---
+++ sourc.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+
+yes|apt update
+yes|apt upgrade
+yes|pkg install wget
+yes|pkg install neofetch
+yes|pkg install fish
+yes|pkg install ruby
+gem install lolcat
+yes|pkg install figlet
+
+w0,
